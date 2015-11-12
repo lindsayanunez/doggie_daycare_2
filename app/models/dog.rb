5f90c8ebfa27f26687b46ccf4133cd0394295663
Lindsay Nunez <lindsayanunez@gmail.com>
2015-11-12 09:36:57 -0600

    Added file uploads with PaperClip

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,9 @@ class Dog < ActiveRecord::Base
   belongs_to :breed
   belongs_to :owner
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   validates :name, :breed_id, :owner_id, :dob, presence: true
 end
 
@@ -9,15 +12,19 @@ end
 #
 # Table name: dogs
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  breed_id   :integer
-#  owner_id   :integer
-#  med_cond   :string
-#  vet        :string
-#  dob        :date
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                  :integer          not null, primary key
+#  name                :string
+#  breed_id            :integer
+#  owner_id            :integer
+#  med_cond            :string
+#  vet                 :string
+#  dob                 :date
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
 #
 # Indexes
 #
